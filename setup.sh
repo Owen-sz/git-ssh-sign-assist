ba08327c384ceb19d0e0382d1ff93552b442cb24
Owen <owen@fyralabs.com>
2025-10-21 19:49:03 -0500

    colors
    Signed-off-by: Owen <owen@fyralabs.com>

--- setup.sh
+++ setup.sh
@@ -1,39 +1,46 @@
 #!/usr/bin/env bash
 
-echo "You have started the git ssh setup script"
+BOLD='\033[1m'
+BOLD2='\133[1m'
+BOLD3='\034[1m'
+RESET='\033[0m'
 
-echo "First, let's set up git. What is your GitHub username?"
+echo "${BOLD}You have started the git ssh setup script${RESET}"
+
+echo "${BOLD2}First, let's set up git. What is your GitHub username?${RESET}"
 read -r ghuser
+echo "your GitHub username is ${ghuser}"
 echo "What is your email you would like to use?"
 read -r email
+echo "your email that git will use is ${email}"
 
-echo "Configuring git with your information... "
+echo "${BOLD2}Configuring git with your information...${RESET}"
 git config --global user.name "${ghuser}"
 git config --global user.email "${email}"
 
-echo "Telling git to use autosign commits with ssh..."
+echo "${BOLD2}Telling git to use autosign commits with ssh...${RESET}"
 git config --global gpg.format ssh
 git config --global commit.gpgsign true
 
 SSHKEY=~/.ssh/id_ed25519.pub
 if [ -f "${SSHKEY}" ]; then
-    echo "${SSHKEY} exists, you do not need to generate an SSH key."
+    echo "${BOLD}${SSHKEY} exists, you do not need to generate an SSH key.${RESET}"
 else
-    echo "${SSHKEY} does not exist. You will need to generate an SSH key. Do that now? (Y/N)"
+    echo "${BOLD}${SSHKEY} does not exist. You will need to generate an SSH key. Do that now (note that SSH itself will run you through the keygen. Typically, you can just leave everything blank and press Enter)? (Y/N)${RESET}"
     read -r generate
     if [[ ${generate} = "y" ]]; then
         ssh-keygen
     else
-        echo "Skipping ssh key generation, you will not be able to sign commits unless you generate a key..."
+        echo "${BOLD2}Skipping ssh key generation, you will not be able to sign commits unless you generate a key...${RESET}"
     fi
 fi
 
-echo "Now that you have an SSH key, go to this link: https://github.com/settings/keys, then select 'New SSH key', give the key a name, and paste the below text into the 'Key' box. NOTE: You will need to add this key to the settings again, but this time change 'Key type' to 'Authentication Key'."
+echo "${BOLD}Now that you have an SSH key, go to this link: https://github.com/settings/keys, then select 'New SSH key', give the key a name, and paste the below text into the 'Key' box. NOTE: You will need to add this key to the settings again, but this time change 'Key type' to 'Authentication Key'.${RESET}"
 cat ${SSHKEY}
 
-read -rp "Once completed, press Enter to continue" </dev/tty
+read -rp "${BOLD}Once completed, press Enter to continue${RESET}" </dev/tty
 
-echo "pointing git to your SSH key (${SSHKEY})..."
+echo "${BOLD2}pointing git to your SSH key (${SSHKEY})...${RESET}"
 git config --global user.signingkey ~/.ssh/id_ed25519.pub
 
-echo "All done! Your git commits should be automatically signed with an SSH key now."
+echo "${BOLD3}All done! Your git commits should be automatically signed with an SSH key now.${RESET}"
