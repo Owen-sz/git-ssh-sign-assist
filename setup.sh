f02fdf05aa31da053d8b9db3b2c92d49bdd30d34
Owen <owen@fyralabs.com>
2025-10-21 20:05:14 -0500

    just don't use read to get ambiguous continue, also implement spacing
    Signed-off-by: Owen <owen@fyralabs.com>

--- setup.sh
+++ setup.sh
@@ -5,12 +5,16 @@ RESET='\033[0m'
 
 echo -e "${BOLD}You have started the git ssh setup script${RESET}"
 
+echo ""
+
 echo -e "${BOLD}First, let's set up git. What is your GitHub username?${RESET}"
 read -r ghuser
 echo "your GitHub username is ${ghuser}"
 echo -e "${BOLD}What is your email you would like to use?${RESET}"
 read -r email
 echo "your email that git will use is ${email}"
+
+echo ""
 
 echo -e "${BOLD}Configuring git with your information...${RESET}"
 git config --global user.name "${ghuser}"
@@ -19,6 +23,8 @@ git config --global user.email "${email}"
 echo -e "${BOLD}Telling git to use autosign commits with ssh...${RESET}"
 git config --global gpg.format ssh
 git config --global commit.gpgsign true
+
+echo ""
 
 SSHKEY=~/.ssh/id_ed25519.pub
 if [ -f "${SSHKEY}" ]; then
@@ -33,12 +39,21 @@ else
     fi
 fi
 
+echo ""
+
 echo -e "${BOLD}Now that you have an SSH key, go to this link: https://github.com/settings/keys, then select 'New SSH key', give the key a name, and paste the below text into the 'Key' box. NOTE: You will need to add this key to the settings again, but this time change 'Key type' to 'Authentication Key'.${RESET}"
 cat ${SSHKEY}
 
-read -r -p "${BOLD}Once completed, press Enter to continue${RESET}" </dev/tty
+echo ""
+
+echo -e "${BOLD}Once completed, press Enter to continue${RESET}"
+read completed
+
+echo ""
 
 echo -e "${BOLD}pointing git to your SSH key (${SSHKEY})...${RESET}"
 git config --global user.signingkey ~/.ssh/id_ed25519.pub
+
+echo ""
 
 echo -e "${BOLD}All done! Your git commits should be automatically signed with an SSH key now.${RESET}"
